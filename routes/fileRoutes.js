f20f1bc38564647554c4c417b5879fcdc8e66587
akashtharuka <akash_tharuka@yahoo.com>
2024-05-06 00:31:26 +0530

    file routes to get and send chunks

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -7,4 +7,10 @@ const router = express.Router();
 //personal client register
 // router.get('/test1', testController.test_get);
 
+// send chunks from file server
+router.post('/send-chunk', fileController.sendChunk);
+
+// get chunks from file server
+router.get('/get-chunk', fileController.getChunk);
+
 module.exports = router;
