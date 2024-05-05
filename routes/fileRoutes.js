a144c96fc199b811f1fc387a4ce21e3d55fe588c
akashtharuka <akash_tharuka@yahoo.com>
2024-05-06 00:48:15 +0530

    route names changed

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,14 +1,10 @@
 const express = require('express');
-
 const fileController = require('../controllers/fileController');
 
 const router = express.Router();
 
-//personal client register
-// router.get('/test1', testController.test_get);
-
 // send chunks from file server
-router.post('/send-chunk', fileController.sendChunk);
+router.post('/save-chunk', fileController.saveChunk);
 
 // get chunks from file server
 router.get('/get-chunk', fileController.getChunk);
