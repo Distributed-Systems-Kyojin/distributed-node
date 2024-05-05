2a117f725e45db0e62304e8d3d920f79170f1cf0
akashtharuka <akash_tharuka@yahoo.com>
2024-05-06 01:02:41 +0530

    updated getChunk saveChunk methods to suit best practices

--- services/fileService.js
+++ services/fileService.js
@@ -2,29 +2,33 @@ const db = require('../db_connection').openDatabase();
 
 // save chunk data to the database
 const saveChunk = async (chunkId, fileName, chunkIndex, chunkData) => {
-    try {
-        db.run('INSERT INTO ChunkData (chunkID, fileName, chunkIndex, chunkData, createdAt) VALUES (?, ?, ?, ?, ?)', [chunkId, fileName, chunkIndex, chunkData, new Date().toISOString()], function (err) {
+    const insertQuery = 'INSERT INTO ChunkData (chunkID, fileName, chunkIndex, chunkData, createdAt) VALUES (?, ?, ?, ?, ?)';
+    const params = [chunkId, fileName, chunkIndex, chunkData, new Date().toISOString()];
+    return new Promise((resolve, reject) => {
+        db.run(insertQuery, params, function(err) {
             if (err) {
-                console.error('Error saving chunk data:', err);
+                console.error(`Error saving chunk data: ${err}`);
+                return reject(err);
             }
-            else {
-                console.log(`Saved chunk ${chunkIndex} for ${fileName}`);
-            }
+
+            console.log(`Saved chunk ${chunkIndex} for ${fileName}`);
+            resolve();
         });
-    } catch (err) {
-        console.error('Error saving chunk data:', err);
-    }
+    });
 }
 
 // get saved chunk data from id and filename
 const getChunk = async (chunkId, fileName) => {
+    const selectQuery = 'SELECT * FROM ChunkData WHERE chunkID = ? AND fileName = ?';
+    const params = [chunkId, fileName];
     return new Promise((resolve, reject) => {
-        db.get('SELECT * FROM ChunkData WHERE chunkID = ? AND fileName = ?', [chunkId, fileName], (err, row) => {
+        db.get(selectQuery, params, (err, row) => {
             if (err) {
                 console.error('Error getting chunk data:', err);
                 reject(err);
             }
             else {
+                console.log(`Retrieved chunk data for ${fileName} with id ${chunkId}`);
                 resolve(row);
             }
         });
